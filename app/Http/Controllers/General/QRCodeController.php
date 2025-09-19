63a8798732498d5078ab927ed2ba0eecaa6a78c2
astrogeeksagar <sagarrajpoot7860@gmail.com>
2025-09-19 15:13:09 +0530

    logo updqated

--- app/Http/Controllers/General/QRCodeController.php
+++ app/Http/Controllers/General/QRCodeController.php
@@ -35,18 +35,44 @@ class QRCodeController extends Controller
                 'success' => false
             ], 401);
         }
+
         $compdata = companydata();
+        $url = url("/outlet/{$compdata->propertyid}/{$chk->dcode}/" . str_replace(' ', '_', $compdata->comp_name));
 
-        $url = url("/outlet/{$compdata->propertyid}/{$chk->dcode}/{$compdata->comp_name}");
+        $qrImage = QrCode::format('png')->size(512)->margin(1)->generate($url);
 
-        $icon = "/public/admin/images/favicon.png";
+        $qr = imagecreatefromstring($qrImage);
 
-        $qrcode = QrCode::size(512)
-            ->format('png')
-            ->merge($icon)
-            ->errorCorrection('M')
-            ->generate($url);
+        $iconPath = public_path('admin/images/favicon.png');
+        $icon = imagecreatefrompng($iconPath);
 
-        return response($qrcode)->header('Content-Type', 'image/png');
+        $qrWidth = imagesx($qr);
+        $qrHeight = imagesy($qr);
+        $iconWidth = imagesx($icon);
+        $iconHeight = imagesy($icon);
+
+        $scale = 0.2;
+        $newIconWidth = $qrWidth * $scale;
+        $newIconHeight = ($iconHeight / $iconWidth) * $newIconWidth;
+
+        $resizedIcon = imagecreatetruecolor($newIconWidth, $newIconHeight);
+        imagecolortransparent($resizedIcon, imagecolorallocatealpha($resizedIcon, 0, 0, 0, 127));
+        imagealphablending($resizedIcon, false);
+        imagesavealpha($resizedIcon, true);
+        imagecopyresampled($resizedIcon, $icon, 0, 0, 0, 0, $newIconWidth, $newIconHeight, $iconWidth, $iconHeight);
+
+        $dstX = ($qrWidth - $newIconWidth) / 2;
+        $dstY = ($qrHeight - $newIconHeight) / 2;
+        imagecopy($qr, $resizedIcon, $dstX, $dstY, 0, 0, $newIconWidth, $newIconHeight);
+
+        ob_start();
+        imagepng($qr);
+        $finalImage = ob_get_clean();
+
+        imagedestroy($qr);
+        imagedestroy($icon);
+        imagedestroy($resizedIcon);
+
+        return response($finalImage)->header('Content-Type', 'image/png');
     }
 }
