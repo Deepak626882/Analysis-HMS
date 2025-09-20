d5bdea522a7801a41c2377af8e9c95ecbf72ff80
astrogeeksagar <sagarrajpoot7860@gmail.com>
2025-09-20 15:43:52 +0530

    logo fun added

--- resources/views/admin/companies.blade.php
+++ resources/views/admin/companies.blade.php
@@ -57,58 +57,75 @@
                                             <th>City</th>
                                             <th>Mobile</th>
                                             <th>Email</th>
+                                            <th>Logo</th>
                                             <th>Action</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                            @foreach ($companies as $list)
-                                                <tr>
-                                                    <td>{{ $list->propertyid }}</td>
-                                                    <td>{{ $list->comp_name }}</td>
-                                                    <td>{{ $list->u_name }}</td>
-                                                    <td>{{ date('d-m-Y', strtotime($list->start_dt)) }}</td>
-                                                    <td>{{ date('d-m-Y', strtotime($list->end_dt)) }}</td>
-                                                    <td>{{ $list->country }}</td>
-                                                    <td>{{ $list->state }}</td>
-                                                    <td>{{ $list->city }}</td>
-                                                    <td><a class="text-dark"
-                                                            href="tel:{{ $list->mobile }}">{{ $list->mobile }}</a></td>
-                                                    <td><a class="text-dark"
-                                                            href="mailto:{{ $list->email }}">{{ $list->email }}</a></td>
-                                                    <td class="ins">
-                                                        <a
-                                                            href="updatepropertyadmin?propertyid={{ base64_encode($list->propertyid) }}">
-                                                            <button class="btn-success btn-sm btn">Update <i
-                                                                    class="fa-regular fa-pen-to-square"></i></button>
-                                                        </a>
-                                                        @php
-                                                            if ($list->status == 1) {
-                                                                $encodedPropertyId = base64_encode($list->propertyid);
-                                                                echo '<a href="#" onclick="return confirmBan(\'' . $encodedPropertyId . '\')"><button class="btn-danger btn-sm text-white btn">InActive <i class="fa-solid fa-ban"></i></button></a>';
-                                                            } else {
-                                                                $encodedPropertyId = base64_encode($list->propertyid);
-                                                                echo '<a href="#" onclick="return confirmUnban(\'' . $encodedPropertyId . '\')"><button class="btn-info btn-sm text-white btn">Active <i class="fa-solid fa-user-check"></i></button></a>';
-                                                            }
-                                                        @endphp
-                                                    </td>
-                                                </tr>
-                                            @endforeach
+                                        @foreach ($companies as $list)
+                                            <tr>
+                                                <td>{{ $list->propertyid }}</td>
+                                                <td>{{ $list->comp_name }}</td>
+                                                <td>{{ $list->u_name }}</td>
+                                                <td>{{ date('d-m-Y', strtotime($list->start_dt)) }}</td>
+                                                <td>{{ date('d-m-Y', strtotime($list->end_dt)) }}</td>
+                                                <td>{{ $list->country }}</td>
+                                                <td>{{ $list->state }}</td>
+                                                <td>{{ $list->city }}</td>
+                                                <td><a class="text-dark"
+                                                        href="tel:{{ $list->mobile }}">{{ $list->mobile }}</a></td>
+                                                <td><a class="text-dark"
+                                                        href="mailto:{{ $list->email }}">{{ $list->email }}</a></td>
+                                                <td>
+                                                    <img
+                                                        data-bs-toggle="modal"
+                                                        data-bs-target="#logomodal"
+                                                        style="width: 10vh; cursor: pointer;"
+                                                        onerror="this.src='https://placehold.co/25x25?text=A+S'"
+                                                        src="{{ url('/storage/admin/property_logo') }}/{{ $list->logo }}"
+                                                        alt="{{ $list->comp_name }}"
+                                                        onclick="showLogo('{{ url('/storage/admin/property_logo') }}/{{ $list->logo }}', '{{ $list->comp_name }}', '{{ $list->logo }}')">
+                                                </td>
+                                                <td class="ins">
+                                                    <a
+                                                        href="updatepropertyadmin?propertyid={{ base64_encode($list->propertyid) }}">
+                                                        <button class="btn-success btn-sm btn">Update <i
+                                                                class="fa-regular fa-pen-to-square"></i></button>
+                                                    </a>
+                                                    @php
+                                                        if ($list->status == 1) {
+                                                            $encodedPropertyId = base64_encode($list->propertyid);
+                                                            echo '<a href="#" onclick="return confirmBan(\'' . $encodedPropertyId . '\')"><button class="btn-danger btn-sm text-white btn">InActive <i class="fa-solid fa-ban"></i></button></a>';
+                                                        } else {
+                                                            $encodedPropertyId = base64_encode($list->propertyid);
+                                                            echo '<a href="#" onclick="return confirmUnban(\'' . $encodedPropertyId . '\')"><button class="btn-info btn-sm text-white btn">Active <i class="fa-solid fa-user-check"></i></button></a>';
+                                                        }
+                                                    @endphp
+                                                </td>
+                                            </tr>
+                                        @endforeach
                                     </tbody>
-                                    {{-- <tfoot class="bg-secondary">
-                                        <tr>
-                                            <th>Property Id</th>
-                                            <th>Company Name</th>
-                                            <th>Username</th>
-                                            <th>Start Dt</th>
-                                            <th>End Dt</th>
-                                            <th>Country</th>
-                                            <th>State</th>
-                                            <th>City</th>
-                                            <th>Mobile</th>
-                                            <th>Email</th>
-                                        </tr>
-                                    </tfoot> --}}
                                 </table>
+                            </div>
+
+                            <div class="modal fade" id="logomodal" tabindex="-1" aria-labelledby="logomodalLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-lg">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h1 class="modal-title fs-5" id="logomodalLabel">Company Logo</h1>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body text-center">
+                                            <img id="companylogo" src="" alt="" class="img-fluid" style="max-width: 100%; height: auto;">
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                            <a id="downloadLogo" href="" download="" class="btn btn-primary">
+                                                <i class="fa-solid fa-download"></i> Download Logo
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -116,4 +133,17 @@
             </div>
         </div>
     </div>
+
+    <script>
+        function showLogo(logoUrl, companyName, logoFilename) {
+            $('#companylogo').attr('src', logoUrl);
+            $('#companylogo').attr('alt', companyName + ' Logo');
+            $('#logomodalLabel').text(companyName + ' Logo');
+            $('#downloadLogo').attr('href', logoUrl);
+            $('#downloadLogo').attr('download', logoFilename);
+
+            var myModal = new bootstrap.Modal(document.getElementById('logomodal'));
+            myModal.show();
+        }
+    </script>
 @endsection
